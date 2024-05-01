8069db30327c3f97a9ae83a6b6466157fc611f11
Nguyen Trong Ninh <nguyentrongninh24072003@gmail.com>
2024-05-01 22:24:19 +0700

    add leader board page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,52 +1,88 @@
-'use client';
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
 
-const Footer: React.FC = () => {
+const Footer = () => {
   return (
-    <footer className="fixed-bottom alert-secondary fw-bold">
-      <div className="row p-1 align-items-center">
-        <div className="col-4">
-          Developed by{' '}
-          <a
-            href="https://github.com/Aditya-ds-1806"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link-secondary"
-          >
-            Aditya D.S.
-          </a>
+    <footer className="bg-black text-white pt-14 pb-3">
+      <div
+        className="w-full px-5
+  md:px-10 mx-auto flex justify-between flex-col md:flex-row gap-[50px] md:gap-0"
+      >
+        {/* LEFT START */}
+        <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
+          {/* MENU START */}
+          <div className="flex flex-col gap-3 shrink-0">
+            <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+              Tìm kiếm cửa hàng
+            </div>
+          </div>
+          {/* MENU END */}
+
+          {/* NORMAL MENU START */}
+          <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+            {/* MENU START */}
+            <div className="flex flex-col gap-3">
+              <div className="font-oswald font-medium uppercase text-sm">
+                Tìm kiếm giúp đỡ
+              </div>
+            </div>
+            {/* MENU END */}
+
+            {/* MENU START */}
+            <div className="flex flex-col gap-3">
+              <div className="font-oswald font-medium uppercase text-sm">
+                về Draw and Guess
+              </div>
+            </div>
+            {/* MENU END */}
+          </div>
+          {/* NORMAL MENU END */}
         </div>
-        <div className="col-4 text-center">
-          Powered by{' '}
-          <a
-            href="https://socket.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/socket.io.png"
-              className="align-text-bottom"
-              width="20"
-              alt="Socket.IO"
-            />
-          </a>
+        {/* LEFT END */}
+
+        {/* RIGHT START */}
+        <div className="flex gap-4 justify-center md:justify-start">
+          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaFacebookF size={20} />
+          </div>
+          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaTwitter size={20} />
+          </div>
+          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaYoutube size={20} />
+          </div>
+          <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaInstagram size={20} />
+          </div>
         </div>
-        <div className="col-4 align-right text-end align-self-center">
-          <a
-            href="https://github.com/Aditya-ds-1806/Skribblrs.io"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link-secondary"
-          >
-            View on GitHub{' '}
-            <img
-              src="/images/github.png"
-              className="align-text-bottom"
-              width="20"
-              alt="GitHub"
-            />
-          </a>
+        {/* RIGHT END */}
+      </div>
+      <div
+        className="w-full px-5
+  md:px-10 mx-auto flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0"
+      >
+        {/* LEFT START */}
+        <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
+          © 2024 Draw and Guess, Inc. All Rights Reserved
         </div>
+        {/* LEFT END */}
+
+        {/* RIGHT START */}
+        <div className="flex gap-2 md:gap-5 text-center md:text-left flex-wrap justify-center">
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Guides
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Terms of Sale
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Terms of Use
+          </div>
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+            Privacy Policy
+          </div>
+        </div>
+        {/* RIGHT END */}
       </div>
     </footer>
   );
---
+++ src/components/leaderboard/LeaderBoard.jsx
@@ -0,0 +1,168 @@
+'use client';
+import React, { useEffect, useState, useReducer, useCallback } from 'react';
+import {
+  Progress,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Text,
+  Flex,
+  Avatar,
+  Center,
+  TagRightIcon,
+  VStack,
+  Heading,
+  Select,
+  Button,
+  Tag,
+  Stack,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerCloseButton,
+  DrawerBody,
+  DrawerFooter,
+} from '@chakra-ui/react';
+import {
+  FiArrowDown,
+  FiCheckCircle,
+  FiChevronDown,
+  FiCircle,
+  FiCornerDownRight,
+  FiRefreshCcw,
+} from 'react-icons/fi';
+import { Reorder } from 'framer-motion';
+import LeaderBoardEntry from './LeaderBoardEntry';
+
+export default function LeaderBoard({
+  initTimeline,
+  initProperty,
+  currentData,
+}) {
+  const [timeline, setTimeline] = useState(
+    initTimeline ? initTimeline : 'Lifetime'
+  );
+  const [property, setProperty] = useState(
+    initProperty ? initProperty : 'dollarVolume'
+  );
+  const [sortedData, setSortedData] = useState();
+
+  useEffect(() => {
+    console.log('sorting');
+    const sortData = () => {
+      if (currentData) {
+        if (property == 'Funded') {
+          setSortedData(sortArrayByProperty('noFundedLifetime'));
+        } else setSortedData(sortArrayByProperty('dollarVolumeLifetime'));
+      }
+    };
+    sortData();
+  }, [property, currentData]);
+
+  const sortArrayByProperty = (prop) => {
+    return [...currentData].sort((a, b) => b[`${prop}`] - a[`${prop}`]);
+  };
+
+  return (
+    <>
+      <Flex p={3} flexDir={'column'} w="250px">
+        {sortedData && (
+          <>
+            <Center>
+              <Heading mb={5} as="em" size="md">
+                <Flex flexDir={'column'}>
+                  <Center>
+                    <Flex mb={3} alignItems="center" flexDir="column">
+                      <Progress
+                        mt="2px"
+                        width="100%"
+                        height={'2px'}
+                        colorScheme={'red'}
+                        size="xs"
+                        isIndeterminate
+                      />
+                    </Flex>
+                  </Center>
+                </Flex>
+              </Heading>
+            </Center>
+            {sortedData && (
+              <Flex mb={2} justify={'space-between'} flexDir="row">
+                <Button
+                  p={0}
+                  variant={'ghost'}
+                  size="xs"
+                  onClick={() => setProperty('dollarVolume')}
+                >
+                  <Tag
+                    borderRadius={'3xl'}
+                    m={2}
+                    fontSize="xs"
+                    fontWeight={property == 'dollarVolume' ? 'bold' : 'normal'}
+                    variant={property == 'dollarVolume' ? 'solid' : 'subtle'}
+                  >
+                    Amount
+                    {property == 'dollarVolume' && (
+                      <TagRightIcon as={FiChevronDown} />
+                    )}
+                  </Tag>
+                </Button>
+                <Button
+                  p={0}
+                  variant={'ghost'}
+                  size="xs"
+                  onClick={() => setProperty('Funded')}
+                >
+                  <Tag
+                    borderRadius={'3xl'}
+                    m={2}
+                    fontSize="xs"
+                    fontWeight={property == 'Funded' ? 'bold' : 'normal'}
+                    variant={property == 'Funded' ? 'solid' : 'subtle'}
+                  >
+                    Quantity
+                    {property == 'Funded' && (
+                      <TagRightIcon as={FiChevronDown} />
+                    )}
+                  </Tag>
+                </Button>
+              </Flex>
+            )}
+            <Flex p={2}>
+              <Reorder.Group
+                as="div"
+                draggable={false}
+                dragControls={false}
+                dragListener={false}
+                axis="y"
+                values={sortedData}
+              >
+                {sortedData.map((row) => {
+                  return (
+                    <Reorder.Item
+                      as="div"
+                      key={`${row.LO.Name}`}
+                      dragListener={false}
+                      draggable={false}
+                      value={row}
+                    >
+                      <LeaderBoardEntry
+                        key={`${row.LO.Name}`}
+                        row={row}
+                        property={property}
+                        timeline={timeline}
+                      />
+                    </Reorder.Item>
+                  );
+                })}
+              </Reorder.Group>
+            </Flex>
+          </>
+        )}
+      </Flex>
+    </>
+  );
+}
